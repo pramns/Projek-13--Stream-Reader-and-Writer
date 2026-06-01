f3e1efe8e03e68bd5cfb20a522a9ed48ce87086e
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-06-01 20:03:22 +0700

    perulangan untuk menampilkan setiap baris

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -26,5 +26,9 @@ int main(){
   cout << endl << ">= Membuka dan membaca file" << endl;
     if (infile.is_open())
     {
+      while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
     }
 }
