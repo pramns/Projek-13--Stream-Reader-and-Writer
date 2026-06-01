bee9093b9b2649ffeb5a51ca2745b83abfcfd366
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-06-01 18:43:30 +0700

    kondisi jika terjadi error didalam try

--- exceptionstandart.cpp
+++ exceptionstandart.cpp
@@ -9,7 +9,12 @@ int main(){
     {
       
         array <int, 3> data = { 1, 2, 3 };
-        
         cout << data.at(5) << endl;
     }
+    catch (exception& e)
+    {
+        cout << e.what() << endl;
+    }
+    cout << "Baris program terakhir" << endl; //penanda 2
+    return 0;
 }
